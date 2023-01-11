21472f0c0d3240b0c090ae224cf213ae1e5856fd
ccarlo <55291106+ccarl0@users.noreply.github.com>
2023-01-11 23:26:39 +0100

    Minimal Animation Working on windows, not working on Android

--- AmazingMemoryGame/GamePage.xaml.cs
+++ AmazingMemoryGame/GamePage.xaml.cs
@@ -1,28 +1,32 @@
 ﻿using AmazingMemoryGame.Models;
+using NCalc;
+using System.Linq;
 
 namespace AmazingMemoryGame;
 
 public partial class GamePage : ContentPage
 {
+    List<Button> buttons = new List<Button>();
+    List<Image> buttons_images = new List<Image>();
+    List<string> IDs_list = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "A", "B", "C", "D", "E", "F", "G", "H" };
+     
 
-	public GamePage()
-	{
-		InitializeComponent();
+    public GamePage()
+    {
+        InitializeComponent();
         CreateButtons();
-
     }
+    
 
     private void CreateButtons()
     {
-        
-
         int nOfCards = 16;
         for (int i = 0; i < nOfCards; i++)
         {
             Button button = new Button();
             button.Clicked += OnButtonClicked;
             button.BackgroundColor = Colors.Gray;
-            button.BindingContext = new CardModel { Id = i*2%3, Text = $"Button {i}" };
+            button.BindingContext = new CardModel { Id = IDs_list[i], Text = $"Button {i}", Index=i};
 
             Image backImage = new Image();
             backImage.Source = "dotnet_bot.png";
@@ -36,27 +40,35 @@ public partial class GamePage : ContentPage
 
             mainGrid.Children.Add(button);
             mainGrid.Children.Add(backImage);
+
+            buttons.Add(button);
+            buttons_images.Add(backImage);
         }
     }
 
 
-    private void OnButtonClicked(object sender, EventArgs e)
+    private async void OnButtonClicked(object sender, EventArgs e)
     {
         Button button = (Button)sender;
         var buttonViewModel = (CardModel)button.BindingContext;
-
+        
+        Image backImage = (Image)buttons_images[buttonViewModel.Index];
         // check if the button is already selected
         if (buttonViewModel.IsSelected)
         {
             // deselect the button
             buttonViewModel.IsSelected = false;
             button.BackgroundColor = Colors.Gray;
+            await backImage.ScaleTo(1, 250, Easing.CubicOut);
+            backImage.IsVisible = true;
         }
         else
         {
             // select the button
             buttonViewModel.IsSelected = true;
             button.BackgroundColor = Colors.Red;
+            backImage.IsVisible = true;
+            await backImage.ScaleTo(0, 200, Easing.CubicIn);
             CheckForMatch();
         }
     }
@@ -64,6 +76,7 @@ public partial class GamePage : ContentPage
     private async void CheckForMatch()
     {
         List<Button> selectedButtons = new List<Button>();
+        
         //get all the selected button
         foreach (var child in mainGrid.Children)
         {
@@ -75,8 +88,17 @@ public partial class GamePage : ContentPage
         // check if there are exactly two selected buttons
         if (selectedButtons.Count == 2)
         {
+            foreach (var child in mainGrid.Children)
+            {
+                if (child is Button b)
+                    b.IsEnabled = false;
+            }
+
             var button1 = (CardModel)selectedButtons[0].BindingContext;
             var button2 = (CardModel)selectedButtons[1].BindingContext;
+
+            Image backImage1 = (Image)buttons_images[button1.Index];
+            Image backImage2 = (Image)buttons_images[button2.Index];
 
             if (button1.Id == button2.Id)
             {
@@ -88,15 +110,30 @@ public partial class GamePage : ContentPage
                 selectedButtons[1].Opacity = 20;
                 selectedButtons[0].IsEnabled = false;
                 selectedButtons[1].IsEnabled = false;
+
                 // you can add animation here
             }
             else
             {
+                
                 await Task.Delay(TimeSpan.FromSeconds(1));
                 selectedButtons[0].BackgroundColor = Colors.Gray;
                 selectedButtons[1].BackgroundColor = Colors.Gray;
                 button1.IsSelected = false;
                 button2.IsSelected = false;
+
+                mainGrid.IsEnabled = true;
+
+                await backImage1.ScaleTo(1, 250, Easing.CubicOut);
+                await backImage2.ScaleTo(1, 250, Easing.CubicOut);
+
+                
+            }
+
+            foreach (var child in mainGrid.Children)
+            {
+                if (child is Button b)
+                    b.IsEnabled = true;
             }
         }
 
--- AmazingMemoryGame/Models/CardModel.cs
+++ AmazingMemoryGame/Models/CardModel.cs
@@ -8,11 +8,14 @@ namespace AmazingMemoryGame.Models
 {
     public class CardModel
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
+        public int Index { get; set; }
         public string Text { get; set; }
         public bool IsSelected { get; set; }
         public bool IsMatched { get; set; }
         public bool BindingContext { get; set; }
+
+
     }
 
 
